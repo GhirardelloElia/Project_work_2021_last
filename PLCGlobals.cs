7d7cf4cbb0f3e8d0ae0285baeee71c5214488204
Ghirardello_Elia <eliaghirardelloscuola@gmail.com>
2021-07-13 18:44:35 +0200

    Push

--- PLCGlobals.cs
+++ PLCGlobals.cs
@@ -8,40 +8,49 @@ namespace Progetto_Main
 {
     public class PLCGlobals
     {
+        public static readonly string ns = "4";
+
         // Dati per la commessa corrente 
 
-        public static readonly string Codice_commessa_in_corso = "ns=5;i=8";
-        public static readonly string PezziProdotti = "ns=5;i=13";
-        public static readonly string PezziScarti = "ns=5;i=14";
-        public static readonly string PezziMancanti = "ns=5;i=15";
-        public static readonly string PezziDaProdurre = "ns=5;i=9";
-        public static readonly string Codice_prodotto = "ns=5;i=10";
-        public static readonly string Target_velocita = "ns=5;i=11";
-        public static readonly string Codice_cliente = "ns=5;i=12";
+        public static readonly string Codice_commessa_in_corso = $"ns={ns};i=8";
+        public static readonly string PezziDaProdurre = $"ns={ns}i=9";
+        public static readonly string Codice_prodotto = $"ns={ns}i=10";
+        public static readonly string Target_velocita = $"ns={ns}i=11";
+        public static readonly string Codice_cliente = $"ns={ns}i=12";
+        public static readonly string PezziProdotti = $"ns={ns};i=13";
+        public static readonly string PezziScarti = $"ns={ns};i=14";
+        public static readonly string PezziMancanti = $"ns={ns};i=15";
 
         // Dati per il management della coda
 
-        public static readonly string Sovrascrittura = "ns=5;i=25";
-        public static readonly string Codice_commessa_coda = "ns=5;i=20";
-        public static readonly string PezziDaProdurre_coda = "ns=5;i=21";
-        public static readonly string Codice_prodotto_coda = "ns=5;i=22";
-        public static readonly string Target_velocita_coda = "ns=5;i=23";
-        public static readonly string Codice_cliente_coda = "ns=5;i=24";
+        public static readonly string Codice_commessa_coda = $"ns={ns};i=20";
+        public static readonly string PezziDaProdurre_coda = $"ns={ns};i=21";
+        public static readonly string Codice_prodotto_coda = $"ns={ns};i=22";
+        public static readonly string Target_velocita_coda = $"ns={ns};i=23";
+        public static readonly string Codice_cliente_coda = $"ns={ns};i=24";
+        public static readonly string Sovrascrittura = $"ns={ns};i=25";
 
         // Dati comunicazione ufficio
 
         public static readonly string MessaggioPerUfficio = "";
-        public static readonly string MessaggioDaUfficio = "ns=5;i=59";
-        public static readonly string AbilitazioneDaUfficio = "ns=5;i=59";
-
-        // Stato macchina
-
-        public static readonly string StatoMacchina = "ns=5;i=31";
+        public static readonly string MessaggioDaUfficio = $"ns={ns};i=59";
+        public static readonly string AbilitazioneDaUfficio = $"ns={ns};i=59";
+        public static readonly string WatchDog = $"ns={ns};i=191";
 
         // Info generali
 
-        public static readonly string PezziTOT = "ns=5;i=38";
-        public static readonly string OreTOT = "ns=5;i=39";
-        public static readonly string Velocita_attuale = "ns=5;i=40";
+        public static readonly string PezziTOT = $"ns={ns};i=38";
+        public static readonly string OreTOT = $"ns={ns};i=39";
+        public static readonly string Velocita_attuale = $"ns={ns};i=40";
+
+        // Stato macchina
+
+        public static readonly string StatoMacchina = $"ns={ns};i=31"; // Array di booleani?
+        public static readonly string Emergenza_in_corso = $"ns={ns};i=32";
+        public static readonly string Macchina_in_stop = $"ns={ns};i=33";
+        public static readonly string Stop_in_fase = $"ns={ns};i=34";
+        public static readonly string Ciclo_auto_in_corso = $"ns={ns};i=35";
+        public static readonly string Comandi_manuali_inseriti = $"ns={ns};i=36";
+        public static readonly string Produzione_libera = $"ns={ns};i=37";
     }
 }
